c3466fe49db8d4223b6d4888f16e4d46d89cd1db
Sai supriya sadasivuni <saisupriya940@gmail.com>
2021-07-03 20:12:06 +0530

    Add files via upload

---
+++ automorphic.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class automorphic
+{
+public static void main(String args[])
+{
+int n,sq;
+Scanner sc=new Scanner(System.in);
+System.out.println("enter a number");
+n=sc.nextInt();
+sq=n*n;
+if(sq%100==n)
+System.out.println("Automorphic number");
+else
+System.out.println("Not an Automorphic number");
+}}
